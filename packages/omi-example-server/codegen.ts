e1a62e8827650a3c7272adfbe7ddf5a9504739d3
sheason2019 <811627877@qq.com>
2022-08-15 22:13:05 +0800

    feat: 移除example仓库

--- packages/omi-example-server/codegen.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-import OmiCodegen from "omi-codegen";
-
-const content = fs.readFileSync("../omi-example-idl/todo.omi").toString();
-
-const codegen = new OmiCodegen();
-
-codegen.setContent(content);
-codegen.toTypescript("server", "todo", "api-lib");
