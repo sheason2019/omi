99e50e5e0682e7361670ae3b621980e33b935400
sheason2019 <811627877@qq.com>
2022-08-16 17:36:24 +0800

    trouble: 扩展无法生效

--- packages/omi-idl-extension/src/extension.ts
+++ packages/omi-idl-extension/src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { OmiParser } from "omi-ast-parser";
+import { OmiParser } from "@omi-stack/omi-ast-parser";
 
 // 代码高亮
 interface IParsedToken {
@@ -59,15 +59,57 @@ const legend = (function () {
   );
 })();
 
-class DocumentSemanticTokensProvider
-  implements vscode.DocumentSemanticTokensProvider
-{
-  // onDidChangeSemanticTokens?: vscode.Event<void> | undefined;
-  async provideDocumentSemanticTokens(
+function _parseText(text: string, uri: vscode.Uri): IParsedToken[] {
+  const parser = new OmiParser();
+  parser.setContent(text);
+  try {
+    diagnosticCollection.clear();
+    parser.build();
+  } catch (e) {
+    const err = e as Error;
+    const position = parser.getPosition();
+    const currentToken = parser.getCurrentToken();
+    const range = new vscode.Range(
+      position.row,
+      position.col - currentToken.token.length,
+      position.row,
+      position.col
+    );
+    const diagnostic = new vscode.Diagnostic(range, err.message);
+    diagnosticCollection.set(uri, [diagnostic]);
+  }
+  const tokens = parser.getAllToken();
+  return tokens;
+}
+
+function encodeTokenType(tokenType: string): number {
+  if (tokenTypes.has(tokenType)) {
+    return tokenTypes.get(tokenType)!;
+  } else if (tokenType === "notInLegend") {
+    return tokenTypes.size + 2;
+  }
+  return 0;
+}
+
+function encodeTokenModifiers(strTokenModifiers: string[]): number {
+  let result = 0;
+  for (let i = 0; i < strTokenModifiers.length; i++) {
+    const tokenModifier = strTokenModifiers[i];
+    if (tokenModifiers.has(tokenModifier)) {
+      result = result | (1 << tokenModifiers.get(tokenModifier)!);
+    } else if (tokenModifier === "notInLegend") {
+      result = result | (1 << (tokenModifiers.size + 2));
+    }
+  }
+  return result;
+}
+
+const provider: vscode.DocumentSemanticTokensProvider = {
+  provideDocumentSemanticTokens(
     document: vscode.TextDocument,
     token: vscode.CancellationToken
-  ): Promise<vscode.SemanticTokens | null | undefined> {
-    const allTokens = this._parseText(document.getText(), document.uri);
+  ): vscode.SemanticTokens | null | undefined {
+    const allTokens = _parseText(document.getText(), document.uri);
 
     const builder = new vscode.SemanticTokensBuilder();
     allTokens.forEach((token) => {
@@ -75,69 +117,25 @@ class DocumentSemanticTokensProvider
         token.line,
         token.startCharacter,
         token.length,
-        this._encodeTokenType(token.tokenType),
-        this._encodeTokenModifiers(token.tokenModifiers)
+        encodeTokenType(token.tokenType),
+        encodeTokenModifiers(token.tokenModifiers)
       );
     });
     return builder.build();
-  }
-
-  private _encodeTokenType(tokenType: string): number {
-    if (tokenTypes.has(tokenType)) {
-      return tokenTypes.get(tokenType)!;
-    } else if (tokenType === "notInLegend") {
-      return tokenTypes.size + 2;
-    }
-    return 0;
-  }
-
-  private _encodeTokenModifiers(strTokenModifiers: string[]): number {
-    let result = 0;
-    for (let i = 0; i < strTokenModifiers.length; i++) {
-      const tokenModifier = strTokenModifiers[i];
-      if (tokenModifiers.has(tokenModifier)) {
-        result = result | (1 << tokenModifiers.get(tokenModifier)!);
-      } else if (tokenModifier === "notInLegend") {
-        result = result | (1 << (tokenModifiers.size + 2));
-      }
-    }
-    return result;
-  }
-
-  private _parseText(text: string, uri: vscode.Uri): IParsedToken[] {
-    const parser = new OmiParser();
-    parser.setContent(text);
-    try {
-      diagnosticCollection.clear();
-      parser.build();
-    } catch (e) {
-      const err = e as Error;
-      const position = parser.getPosition();
-      const currentToken = parser.getCurrentToken();
-      const range = new vscode.Range(
-        position.row,
-        position.col - currentToken.token.length,
-        position.row,
-        position.col
-      );
-      const diagnostic = new vscode.Diagnostic(range, err.message);
-      diagnosticCollection.set(uri, [diagnostic]);
-    }
-    const tokens = parser.getAllToken();
-    return tokens;
-  }
-}
+  },
+};
 
 // 插件入口
 export function activate(context: vscode.ExtensionContext) {
-  ctx = context;
+  console.log("omi-idl start up");
   diagnosticCollection = vscode.languages.createDiagnosticCollection("omi");
-  ctx.subscriptions.push(diagnosticCollection);
-  context.subscriptions.push(
-    vscode.languages.registerDocumentSemanticTokensProvider(
-      { language: "omi" },
-      new DocumentSemanticTokensProvider(),
-      legend
-    )
-  );
+  context.subscriptions.push(diagnosticCollection);
 }
+
+vscode.languages.registerDocumentSemanticTokensProvider(
+  { language: "omi", scheme: "file" },
+  provider,
+  legend
+);
+
+console.log("any thing");
