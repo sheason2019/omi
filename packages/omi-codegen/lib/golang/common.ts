cddc49b440bf8bd6c0b938feba4f35ab9eba6238
sheason2019 <811627877@qq.com>
2022-11-26 21:23:20 +0800

    fix: fixup

--- packages/omi-codegen/lib/golang/common.ts
+++ packages/omi-codegen/lib/golang/common.ts
@@ -159,7 +159,7 @@ const generateRequestType = (
           md5,
           importFormatMap,
           importUsedMap
-        )} \`${bindMehod}:${item.identify}"${item.repeated ? "[]" : ""}"\``
+        )} \`${bindMehod}:"${item.identify}${item.repeated ? "[]" : ""}"\``
       );
       variableCount++;
     }
